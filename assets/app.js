4ec1aeb3bcbcf8e540cb4d6f4aa3fbd54dbc4e8d
Jordan Nkunga <jnkunga@hotmail.com>
2023-06-13 21:11:14 +0200

    mise en place page d'accueil

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,12 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.scss';
+import './styles/global.scss';
+
+import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
+
+const $ = require('jquery');
+require('bootstrap');
